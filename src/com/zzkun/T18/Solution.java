42f9f1e9ff2c8d33e0f641fabc9ffa248387beae
kun368 <kun368@126.com>
2017-02-07 14:30:47 +0800

    完成T18（今天失恋不爽，多刷点题）

---
+++ src/com/zzkun/T18/Solution.java
@@ -0,0 +1,21 @@
+package com.zzkun.T18;
+
+
+public class Solution {
+
+    private boolean dfsCheck(TreeNode root1, TreeNode root2) {
+        if(root2 == null)
+            return true;
+        return (root1 != null && root1.val == root2.val)
+                && dfsCheck(root1.left, root2.left)
+                && dfsCheck(root1.right, root2.right);
+    }
+
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null)
+            return false;
+        return (root1.val == root2.val && dfsCheck(root1, root2))
+                || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+
+    }
+}
